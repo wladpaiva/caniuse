acb5b9f7784049b308a8ab30302fc74dceda63f1
Wlad Paiva <desenvolvedormg@gmail.com>
2023-12-05 14:27:32 -0300

    add initial models

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,4 +2,52 @@ import { prisma } from "@/lib/prisma";
 
 console.log("🌱 Seeding database...");
 
-// await prisma.user.upsert({})
+const runtimes = ["nodejs", "bun", "edge"];
+
+for (const runtime of runtimes) {
+  await prisma.runtime.upsert({
+    where: { name: runtime },
+    update: {},
+    create: {
+      name: runtime,
+      versions: {
+        // create: versions?.map((v) => ({
+        //   version: v.version,
+        //   features: {
+        //     create: v.features.map((f) => ({
+        //       name: f,
+        //     })),
+        //   },
+        // })),
+      },
+    },
+  });
+
+  console.log(`🚘 Seeded runtime '${runtime}'`);
+}
+
+const packages = ["next", "react", "eslint", "remix", "msw", "playwright"];
+
+for (const p of packages) {
+  await prisma.package.upsert({
+    where: { name: p },
+    update: {},
+    create: {
+      name: p,
+      versions: {
+        // create: versions?.map((v) => ({
+        //   version: v.version,
+        //   features: {
+        //     create: v.features.map((f) => ({
+        //       name: f,
+        //     })),
+        //   },
+        // })),
+      },
+    },
+  });
+
+  console.log(`📦 Seeded package '${p}'`);
+}
+
+await prisma.$disconnect();
